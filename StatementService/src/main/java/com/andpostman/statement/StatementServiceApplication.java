2640ac4b63eaa253e6934c79b1c565f03e8e6f50
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-02-05 16:01:22 +0300

    add StatementService application runner

---
+++ StatementService/src/main/java/com/andpostman/statement/StatementServiceApplication.java
@@ -0,0 +1,12 @@
+package com.andpostman.statement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatementServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StatementServiceApplication.class, args);
+    }
+
+}
